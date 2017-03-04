ba8b9f7874d28848b44cdf5710a91ed455a6ecd8
anna <choclanna@gmail.com>
2017-03-04 19:44:43 +0100

    added the user in the dict to push to database

--- polbotcheck/temp_datatransfer.py
+++ polbotcheck/temp_datatransfer.py
@@ -13,6 +13,7 @@ for user in userlist:
     for follower in followerlist:
         botness = botornotapi.get_bot_or_not(follower)
         if botness is not None:
+            botness['polit']=user  #or any other key, cant think right now
             db.saveFollower(botness)
 
 
