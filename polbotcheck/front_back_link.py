f964ac6a20470c850dc8418c675ec98e6630a350
anna <choclanna@gmail.com>
2017-03-05 16:21:26 +0100

    followers_botness now also returns percentage of follower bots

--- polbotcheck/front_back_link.py
+++ polbotcheck/front_back_link.py
@@ -1,19 +1,28 @@
-
 import db
 import seaborn as sns
+import numpy as np
+
+# takes data (that should come from the frontend) and creates the output we would like to have 
+# on the front end.
 
 
-def follower_botness_histogram(username):
- 
-    scorelist=[]
+
+
+def follower_botness(username):
+"""  given a username, it creates the histogram of the botness of the followers 
+    and saves it in plots (for now)  it also returns the probable percentage of follower bots
+    (cutoff needs to be defined, for now it is 0.7)""" 
+    cutoff = 0.7
+    scorelist = []
     followers = db.getFollowers(toName=username)   
     for f in followers:
         follower = f['_from'].split('/')[1]
         score=db.getUser(follower)['botness']['score']
         scorelist.append(score)
-    ax = sns.distplot(scorelist, name='probability of follower bot')
+    ax = sns.distaplot(scorelist, name='probability of follower bot')
     fig = ax.get_figure()
     fig.savefig('plots/testfig.png')
-
+    botpercent = sum(np.array(scorelist)>cutoff)/len(scorelist)
+    return botpercent
 
 
