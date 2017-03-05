b09d613964a08977fe34befb404cc61e6d788502
Peter Jiang <jollypumpkin@mailbox.org>
2017-03-05 13:28:04 +0100

    fix merge conflict

--- web/src/App.js
+++ web/src/App.js
@@ -1,53 +1,57 @@
 import React, {Component} from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Grid, Row} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
-import {Jumbotron, Tabs, Tab} from 'react-bootstrap';
+import {Jumbotron} from 'react-bootstrap';
 import MemberPage from './Member.js';
 
 class App extends Component {
 
-    state = {};
+  state = {
+    content: 'MEMBER'
+  };
 
-    fetchMemberData() {
-        let data = require('../json/member.json');
-        return data;
-    }
+  fetchMemberData() {
+    let data = require('../json/member.json');
+    return data;
+  }
 
-    componentWillMount() {
-        let newData = this.fetchMemberData();
-        this.setState(newData);
-    }
+  componentWillMount() {
+    let newData = this.fetchMemberData();
+    this.setState(newData);
+  }
 
-    handleSelect() {
-        this.setState(this.state);
-    }
+  render() {
+    return (
+      <Grid className="App">
+        <Row>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>Welcome to PolBotCheck</h2>
+        </Row>
+        <Row>
+          <Jumbotron>
+            <p>Introduction and description</p>
+          </Jumbotron>
+        </Row>
+        <Row>
+          {(()=> {
+            switch (this.state.content) {
+              case 'MEMBER':
+                return <MemberPage member={this.state.member}/>;
+                break;
+              case 'PARTY':
+                return <p>PartyPage</p>;
+              case 'TOPIC':
+                return <p>TopicPage</p>;
+              default:
+                return <p>Unknown Page!</p>;
+            }
+          })()}
 
-    render() {
-        return (
-            <Grid className="App">
-                <Row>
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to PolBotCheck</h2>
-                </Row>
-                <Row>
-                    <Jumbotron>
-                        <p>Introduction and description....</p>
-                    </Jumbotron>
-                </Row>
-                <Row>
-                    <Tabs defaultActiveKey={1}
-                        id="tab"
-                        onSelect={this.handleSelect}>
-                        <Tab eventKey={1} title="Member">
-              {<MemberPage member={this.state.member}/>}</Tab>
-                        <Tab eventKey={2} title="Party">Tab 2 content</Tab>
-                        <Tab eventKey={3} title="Topic">Tab 3 content</Tab>
-                    </Tabs>
-                </Row>
-            </Grid>
-        );
-    }
+        </Row>
+      </Grid>
+    );
+  }
 }
 
 export default App;
--- web/src/Member.js
+++ web/src/Member.js
@@ -3,23 +3,16 @@
  */
 import React, {Component} from 'react';
 import {Col, Row, Panel} from 'react-bootstrap';
+import PieChart from './PieChart';
 
 export default class MemberPage extends Component {
-
-  componentWillMount() {
-    this.state = this.props;
-  }
-
-  componentWillReceiveProps() {
-    this.setState(this.props);
-  }
 
   render() {
     return (
       <div>
         <Col className="App-profile" md={4}>
           <img className="Profile-picture"
-               src="https://upload.wikimedia.org/wikipedia/commons/9/93/Angela_Merkel_2016.jpg"/>
+               src={this.props.member.pictureURL}/>
           <Panel bsStyle="primary"
                  bsSize="large">
             <p>Name: {this.props.member.name ? this.props.member.name : '-' + '\n'}</p>
@@ -31,12 +24,8 @@ export default class MemberPage extends Component {
             <img src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Lowestbirthrates.svg"/>
           </Col>
           <Row>
-            <Col className="Info-followers" md={6}>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/2/2b/Tortendiagramm_Themen_Pabst.jpg"/>
-            </Col>
-            <Col className="Info-retweeters" md={6}>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/2/2b/Tortendiagramm_Themen_Pabst.jpg"/>
-            </Col>
+            <PieChart className="Info-followers col-md-6"></PieChart>
+            <PieChart className="Info-retweeters col-md-6"></PieChart>
           </Row>
         </Col>
       </div>
