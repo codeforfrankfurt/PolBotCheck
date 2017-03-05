008c3be394646b423cb0891f59af7e892aad9a4d
Sebastian Koessler <sebastian.koessler@syrocon.de>
2017-03-05 14:21:18 +0100

    restructure word_cluster

--- polbotcheck/word_cluster.py
+++ polbotcheck/word_cluster.py
@@ -11,7 +11,7 @@ from os import path
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-def get_word_frequencies(words, words_n=10, lang='german'):
+def calc_frequencies(words, words_n=50, lang='german'):
     default_stopwords = set(nltk.corpus.stopwords.words(lang))
     stopwords_file = '../data/stopwords.txt'
     custom_stopwords = set(open(stopwords_file, 'r').read().splitlines())
@@ -27,7 +27,7 @@ def get_word_frequencies(words, words_n=10, lang='german'):
     fdist = nltk.FreqDist(words)
     return fdist.most_common(words_n)
 
-def get_most_common_terms(tweets, words_n=50, lang='english'):
+def get_topic_frequencies(tweets, words_n=50, lang='english'):
     default_stopwords = nltk.corpus.stopwords.words(lang)
     vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words=default_stopwords)
     X = vectorizer.fit_transform(tweets)
@@ -44,23 +44,30 @@ def get_most_common_terms(tweets, words_n=50, lang='english'):
         important_terms.append([w for w, _ in sorted(doc_terms, key=lambda x: x[1])][:5])
     important_terms = [item for sublist in important_terms for item in sublist]
     # print(important_terms)
-    most_common_terms = get_word_frequencies(important_terms, words_n=words_n)
-    return most_common_terms
+    topic_frequencies = calc_frequencies(important_terms, words_n=words_n)
+    return topic_frequencies
 
-if __name__ == "__main__":
-    tweets = []
-    with open('../../../data/tweets.20150430-223406.json') as f:
-        for line in f:
-            tweets.append(json.loads(line)['text'])
-
-    topic_frequencies = get_most_common_terms(tweets, words_n=100)
-    print(topic_frequencies)
-
-    wordcloud = WordCloud().fit_words(topic_frequencies)
+def save_wordcloud(frequencies, filename):
+    wordcloud = WordCloud().fit_words(frequencies)
     fig = plt.figure()
     fig.set_figwidth(12)
     fig.set_figheight(16)
     plt.imshow(wordcloud)
     plt.axis("off")
-    plt.savefig('wordcloud1.png', facecolor='k', bbox_inches='tight')
+    plt.savefig(filename, facecolor='k', bbox_inches='tight')
     print('imaged created')
+
+def load_example_data():
+    tweets = []
+    with open('../../../data/tweets.20150430-223406.json') as f:
+        for line in f:
+            tweets.append(json.loads(line)['text'])
+    return tweets
+
+if __name__ == "__main__":
+    tweets = load_example_data()
+    topic_frequencies = get_topic_frequencies(tweets, words_n=100)
+    print(topic_frequencies)
+
+    save_wordcloud(topic_frequencies, 'wordcloud_uk_election.png')
+
