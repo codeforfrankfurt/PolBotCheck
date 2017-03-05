93adbf5b11ebb4c4c731bcc357e63c7e5d7335f8
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-03-05 13:14:19 +0100

    Fixed syntax (missing closing bracket)

--- web/src/App.js
+++ web/src/App.js
@@ -7,46 +7,47 @@ import MemberPage from './Member.js';
 
 class App extends Component {
 
-  state = {};
+    state = {};
 
-  fetchMemberData() {
-    let data = require('../json/member.json');
-    return data;
-  }
+    fetchMemberData() {
+        let data = require('../json/member.json');
+        return data;
+    }
 
-  componentWillMount() {
-    let newData = this.fetchMemberData();
-    this.setState(newData);
-  }
+    componentWillMount() {
+        let newData = this.fetchMemberData();
+        this.setState(newData);
+    }
 
-  handleSelect() {
-    this.setState(this.state);
-  }
+    handleSelect() {
+        this.setState(this.state);
+    }
 
-  render() {
-    return (
-      <Grid className="App">
-        <Row>
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to PolBotCheck</h2>
-        </Row>
-        <Row>
-          <Jumbotron>
-            <p>Introduction and description....</p>
-          </Jumbotron>
-        </Row>
-        <Row>
-          <Tabs defaultActiveKey={1}
-                id="tab"
-                onSelect={this.handleSelect}>
-            <Tab eventKey={1} title="Member">
+    render() {
+        return (
+            <Grid className="App">
+                <Row>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to PolBotCheck</h2>
+                </Row>
+                <Row>
+                    <Jumbotron>
+                        <p>Introduction and description....</p>
+                    </Jumbotron>
+                </Row>
+                <Row>
+                    <Tabs defaultActiveKey={1}
+                        id="tab"
+                        onSelect={this.handleSelect}>
+                        <Tab eventKey={1} title="Member">
               {<MemberPage member={this.state.member}/>}</Tab>
-            <Tab eventKey={2} title="Party">Tab 2 content</Tab>
-            <Tab eventKey={3} title="Topic">Tab 3 content</Tab>
-          </Tabs>
-        </Row>
-      </Grid>
-    );
-  }
+                        <Tab eventKey={2} title="Party">Tab 2 content</Tab>
+                        <Tab eventKey={3} title="Topic">Tab 3 content</Tab>
+                    </Tabs>
+                </Row>
+            </Grid>
+        );
+    }
+}
 
 export default App;
