91bd57fd01774cebcad0dc471528ef7c36acb87c
aCandidMind <opensource@candidminds.com>
2017-09-20 01:05:08 +0200

    Skip another account which takes a while to process bc of many followers

--- polbotcheck/config/test_candidates.py
+++ polbotcheck/config/test_candidates.py
@@ -1,3 +1,3 @@
 SLUGS = ['peter-tauber-cdu-he', 'joana-cotar-afd-he', 'jens-zimmermann-spd-he']
-SLUGS_TO_SKIP = ['joana-cotar-afd-he']
+SLUGS_TO_SKIP = ['joana-cotar-afd-he', 'peter-tauber-cdu-he']
 FOLLOWER_LIMIT = 0
