ab36b570556c69042cd42bf1f63d8dfcd85e100f
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-09-22 21:44:34 +0200

    Better alignment for title

--- src/Title.js
+++ src/Title.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react'
-import {Row} from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 import logo from '../public/logo.png'
 import './Title.css'
 
 class Title extends Component {
     render() {
-        return (<Row>
+        return (<Row><Col xs={12}>
             <a href="http://codefor.de/frankfurt">
                 <img className="App-logo" src={logo} alt="Logo von Code for Frankfurt"/>
             </a>
             <h1 className="main-title">BotOrNot hessische BTW-Kandidaten 2017</h1>
-        </Row>)
+        </Col></Row>)
     }
 }
 
